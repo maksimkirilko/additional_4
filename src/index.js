0c92a9653440615db21d81455d19a70ecdcb9c81
Maksim Kirilko <maksim.kirilko@intellectsoft.net>
2017-09-18 23:47:38 +0300

    Implement multiply function, that for given two numbers, presented as strings will return their product

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-module.exports = function multiply(first, second) {
-  // your solution
-}
+module.exports = (first, second) => {
+
+    const firstArray = first.split('').reverse();
+    const secondArray = second.split('').reverse();
+    const result = [];
+
+    firstArray.forEach((faElement, i) => {
+        secondArray.forEach((saElement, j) => {
+            if (!result[i + j]) {
+                result[i + j] = 0;
+            }
+            result[i + j] += faElement * saElement;
+        });
+    });
+
+    for (let i = 0; result[i] >= 0; i++) {
+        if (result[i] >= 10) {
+            if (!result[i + 1]) {
+                result[i + 1] = 0;
+            }
+            result[i + 1] += parseInt(result[i] / 10);
+            result[i] %= 10;
+        }
+    }
+
+    return result.reverse().join('');
+};
